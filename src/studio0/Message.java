f0070dc9faa924a1a69bdcf2fa4f71e9f2169974
jessicagusdorff <jessicagusdorff@localhost>
2023-01-19 13:38:35 -0600

    2

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -1,6 +1,6 @@
 package studio0;
 
-public class Message {Hello my name is Jervie
+public class Message {Hello my name is Jervie. hi
 
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
