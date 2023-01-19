61340e6cebf7bda964a4d0c702b10e60a42d5032
huangye <huangye@huangyedeair.dhcp.wustl.edu>
2023-01-19 13:33:56 -0600

    1

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -1,6 +1,6 @@
 package studio0;
 
-public class Message {
+public class Message {Hello my name is Jervie
 
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
